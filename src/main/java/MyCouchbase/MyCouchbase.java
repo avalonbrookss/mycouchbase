d0dd0cb73cb61a7898c06aeb1336228beb0a5323
Stephen Brooks <stephenbrooks@Stephens-MBP.home>
2019-01-10 23:51:15 -0500

    updating queries

--- src/main/java/MyCouchbase/MyCouchbase.java
+++ src/main/java/MyCouchbase/MyCouchbase.java
@@ -21,15 +21,24 @@ public class MyCouchbase {
 		cluster.authenticate("american", "password");
 		Bucket bucket = cluster.openBucket("american");
 
+		// Retrieve a valid address from db to search for in Query #1 for address
+		N1qlQueryResult addrArgResponse = bucket.query(
+			N1qlQuery.simple("SELECT addr.street " +
+				"FROM american " +
+					"UNNEST home_address AS addr " +
+						"WHERE addr.address_id=251;")
+		);
+		String addressArg = "'" + addrArgResponse.allRows().get(0).value().get("street").toString() + "'";
+
 		// 1. N1ql Query for specific address 
-		spacerRows("Cardholder by their ADDRESS");
+		spacerRows("Cardholder by their ADDRESS \n \t \t searching for street = " + addressArg);
 		LocalDateTime time1 = LocalDateTime.now();
 		N1qlQueryResult addressResult = bucket.query(
 			N1qlQuery.simple("SELECT * " + 
 				"FROM american AS cardholder " +
 				"WHERE ANY cardAddress " +
 				"IN cardholder.home_address " +
-				"SATISFIES cardAddress.street='2116 Philip Mount' END;")
+				"SATISFIES cardAddress.street=" + addressArg + " END;")
 		);
 		LocalDateTime time2 = LocalDateTime.now();
 		printRows(addressResult);
@@ -37,13 +46,14 @@ public class MyCouchbase {
 		
 		
 		// 2. N1ql Query for elements within Date range 
-		spacerRows("TIMESTAMP within Date Range");
+		spacerRows("TIMESTAMP within Date Range: \n \t \t  2018-10-04 TO 2018-10-05");
 		LocalDateTime time21 = LocalDateTime.now();
 		N1qlQueryResult timestampResult = bucket.query(
 			N1qlQuery.simple("SELECT single_charge " +
 							 "FROM american AS cardholder " +
 								 "UNNEST cardholder.customer_charges AS single_charge " +
-									 "WHERE single_charge.charge_timestamp BETWEEN '2018-10-04 %' AND '2018-10-05';")
+									 "WHERE single_charge.charge_timestamp BETWEEN '2018-10-04 %' AND '2018-10-05' " +
+											 "ORDER BY single_charge.charge_id ASC;")
 		);
 		LocalDateTime time3 = LocalDateTime.now();
 		printRows(timestampResult);
@@ -51,15 +61,15 @@ public class MyCouchbase {
 		
 		
 		// 3. N1ql Query using parameterized function
-		spacerRows("Parameterized Search");
-		LocalDateTime time31 = LocalDateTime.now();		
-		N1qlQueryResult parameterizedResult = bucket.query(
-			N1qlQuery.parameterized("SELECT * FROM american WHERE email = $email;",
-					JsonObject.create().put("email", "Mina22@hotmail.com"))
-		);
-		LocalDateTime time4 = LocalDateTime.now();
-		printRows(parameterizedResult);
-		printTimeElapsed(time31, time4);
+		// spacerRows("Parameterized Search");
+// 		LocalDateTime time31 = LocalDateTime.now();
+// 		N1qlQueryResult parameterizedResult = bucket.query(
+// 			N1qlQuery.parameterized("SELECT * FROM american WHERE email = $email;",
+// 					JsonObject.create().put("email", "Mina22@hotmail.com"))
+// 		);
+// 		LocalDateTime time4 = LocalDateTime.now();
+// 		printRows(parameterizedResult);
+// 		printTimeElapsed(time31, time4);
 		
 	}
 	
